802f3391597b913537dbd6fa83c29f10943c8781
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-05-07 01:28:37 +0300

    json-server,solution state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect,useState } from "react";
 
 function App() {
+  const [solution,setSolution]= useState(null);
+
+useEffect(()=>{
+  fetch("http://localhost:3001/solutions")
+  .then(response => response.json())
+  .then(data => 
+    //random int between 0-14 for solutions db
+    {const randomSolution = data[Math.floor(Math.random()*data.length)]
+      setSolution(randomSolution)
+    })
+},[setSolution])
+
+
   return (
     <div>
           <h1>React Wordle Clone</h1>
+          {solution && <div>Solution is : {solution.word}</div>}
     </div>
   );
 }
