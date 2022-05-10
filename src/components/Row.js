876921ea71680e923975ce43354a91923b3c1f52
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-05-10 21:57:59 +0300

    Letter Grid Components

---
+++ src/components/Row.js
@@ -0,0 +1,12 @@
+
+export default function Row() {
+  return (
+    <div className="row">
+      <div>A</div>
+      <div>B</div>
+      <div>C</div>
+      <div>D</div>
+      <div>e</div>
+    </div>
+  )
+}
